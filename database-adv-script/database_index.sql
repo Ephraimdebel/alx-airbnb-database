8a413ba170e9ed5616e3f44ffd3733c7d77cd7ff
Ephraim Debel <ephraimdebel@gmail.com>
2025-09-07 03:43:30 +0300

    explani and analyse added

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,17 +1,58 @@
--- Indexes for optimization
+-- ================================
+-- USER TABLE
+-- ================================
 
--- User table
+-- Before index
+EXPLAIN ANALYZE
+SELECT * 
+FROM User u
+WHERE u.email = 'test@example.com';
+
+-- Create index
 CREATE INDEX idx_user_email ON User(email);
-CREATE INDEX idx_user_id ON User(user_id);
 
--- Booking table
+-- After index
+EXPLAIN ANALYZE
+SELECT * 
+FROM User u
+WHERE u.email = 'test@example.com';
+
+
+-- ================================
+-- BOOKING TABLE
+-- ================================
+
+-- Before index
+EXPLAIN ANALYZE
+SELECT * 
+FROM Booking b
+WHERE b.user_id = 1;
+
+-- Create index
 CREATE INDEX idx_booking_user_id ON Booking(user_id);
-CREATE INDEX idx_booking_property_id ON Booking(property_id);
-CREATE INDEX idx_booking_start_date ON Booking(start_date);
 
--- Property table
+-- After index
+EXPLAIN ANALYZE
+SELECT * 
+FROM Booking b
+WHERE b.user_id = 1;
+
+
+-- ================================
+-- PROPERTY TABLE
+-- ================================
+
+-- Before index
+EXPLAIN ANALYZE
+SELECT * 
+FROM Property p
+WHERE p.location = 'Addis Ababa';
+
+-- Create index
 CREATE INDEX idx_property_location ON Property(location);
-CREATE INDEX idx_property_id ON Property(property_id);
 
--- Review table
-CREATE INDEX idx_review_property_id ON Review(property_id);
+-- After index
+EXPLAIN ANALYZE
+SELECT * 
+FROM Property p
+WHERE p.location = 'Addis Ababa';
