166ef48850cc493c218c15af2b1107997da334d6
Ephraim Debel <ephraimdebel@gmail.com>
2025-09-07 04:10:13 +0300

    optimization

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -58,3 +58,26 @@ JOIN Property p USING(property_id)
 LEFT JOIN Payment pay USING(booking_id)
 WHERE b.start_date >= '2025-01-01'  
 ORDER BY b.start_date;
+
+
+-- Optimized query with AND
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    u.user_id,
+    u.first_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.status AS payment_status
+FROM Booking b
+JOIN User u USING(user_id)
+JOIN Property p USING(property_id)
+LEFT JOIN Payment pay USING(booking_id)
+WHERE b.start_date >= '2025-01-01'
+  AND b.status = 'confirmed'
+ORDER BY b.start_date;
